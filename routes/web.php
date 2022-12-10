9ebf00d923634caf49bee8e0ed68e33a0d26dc56
KENTA-MITANI <mitani.kenta88@gmail.com>
2022-12-10 16:17:33 +0900

    P60 リスト3-1

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::get('hello', function() {
+    return view('hello.index');
+});
 
 Route::get('/', function () {
     return view('welcome');
