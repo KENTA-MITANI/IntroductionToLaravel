890fad5e83a4c502695a64b46978fbbfd86165ec
KENTA-MITANI <mitani.kenta88@gmail.com>
2022-12-10 15:50:22 +0900

    P52 リスト2-15

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('hello/{id?}/{pass?}', 'App\Http\Controllers\HelloController@index');
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
 
 Route::get('/', function () {
     return view('welcome');
