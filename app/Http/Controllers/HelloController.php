6f4acb994049c819ae540001e1676735104cd00e
KENTA-MITANI <mitani.kenta88@gmail.com>
2022-12-16 07:21:34 +0900

    P96 リスト3-31〜33

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
     public function index()
     {
-        return view('hello.index');
+        $data = [
+            ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
+            ['name'=>'田中はなこ', 'mail'=>'hanako@flower'],
+            ['name'=>'鈴木さちこ', 'mail'=>'sachico@happy'],
+        ];
+        return view('hello.index', ['data'=>$data]);
     }
 
     public function post(Request $request)
---
+++ resources/views/components/item.blade.php
@@ -0,0 +1,2 @@
+<!-- itemという変数からnameとmailの値を取り出して表示する -->
+<li>{{ $item['name'] }} [{{ $item['mail'] }}]</li>
