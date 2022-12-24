6937a3c709adfe439c256f3077c6c2eacd5488d0
KENTA-MITANI <mitani.kenta88@gmail.com>
2022-12-24 23:23:11 +0900

    P119 リスト4-11〜13

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::get('hello', 'App\Http\Controllers\HelloController@index')
+    ->middleware('helo');
