00db20bb77636c044d5661927a3cfc709012861a
KENTA-MITANI <mitani.kenta88@gmail.com>
2022-12-25 21:35:28 +0900

    P122 リスト4-14〜18

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,9 +8,28 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです。</p>
-    <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-    <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
+    <p>{{ $msg }}</p>
+    @if (count($errors) > 0)
+    <p>入力に問題があります。再入力してください。</p>
+    @endif
+    <form actoin="/hello" method="post">
+        <table>
+            @csrf
+            @if ($errors->has('name'))
+            <tr><th>ERROR</th><td>{{ $errors->first('name') }}</td></th></tr>
+            @endif
+            <tr><th>name: </th><td><input type="text" name="name" value="{{ old('mail') }}"></th></tr>
+            @if ($errors->has('mail'))
+            <tr><th>ERROR</th><td>{{ $errors->first('mail') }}</td></th></tr>
+            @endif
+            <tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></th></tr>
+            @if ($errors->has('age'))
+            <tr><th>ERROR</th><td>{{ $errors->first('age') }}</td></th></tr>
+            @endif
+            <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></th></tr>
+            <tr><th></th><td><input type="submit" vlaue="send"></td></tr>
+        </table>
+    </form>
 @endsection
 
 @section('footer')
