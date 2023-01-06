d6c970697dd6c5f3170d334d55d945e01599d6b6
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-01-06 09:43:29 +0900

    P147 リスト4-25

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\HelloRequest;
 use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['msg'=>'フォームを入力：']);
+        $validator = Validator::make($request->query(), [
+            'id' => 'required',
+            'pass' => 'email',
+        ]);
+
+        if($validator->fails()) {
+            $msg = 'クエリーに問題があります。';
+        } else {
+            $msg = 'ID/PASSを受け付けました。フォームを入力してください。';
+        }
+        return view('hello.index', ['msg'=>$msg,]);
     }
 
     public function post(Request $request)
