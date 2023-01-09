3e330d213c7186a3cd04ac628286fc337d1641a1
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-01-09 16:28:51 +0900

    P152 リスト4-28〜31

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,59 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\HelloRequest;
 use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $validator = Validator::make($request->query(), [
-            'id' => 'required',
-            'pass' => 'email',
-        ]);
-
-        if($validator->fails()) {
-            $msg = 'クエリーに問題があります。';
-        } else {
-            $msg = 'ID/PASSを受け付けました。フォームを入力してください。';
-        }
-        return view('hello.index', ['msg'=>$msg,]);
+        return view('hello.index', ['msg'=>'フォームを入力してください。']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric',
-        ];
-
-        $messages = [
-            'name.required' => '名前は必ず入力してください。',
-            'mail.email' => 'メールアドレスが必要です。',
-            'age.numeric' => '年齢を整数で入力してください。',
-            'age.min' => '年齢はゼロ歳以上で記入してください。',
-            'age.max' => '年齢は200歳以下で入力してください。',
-        ];
-
-        $validator = Validator::make($request->all(), $rules, $messages);
-
-        $validator->sometimes('age', 'min:0', function($input){
-            // is_numeric関数で値が数値かどうかを判断する
-            return is_numeric($input->age);
-        });
-
-        $validator->sometimes('age', 'max:200', function($input){
-            // is_numeric関数で値が数値かどうかを判断する
-            return is_numeric($input->age);
-        });
-
-        if ($validator->fails()) {
-            return redirect('/hello')
-                        ->withErrors($validator)
-                        ->withInput();
-        }
-
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
