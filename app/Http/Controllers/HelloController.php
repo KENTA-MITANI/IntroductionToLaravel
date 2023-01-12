255ec3b1dffba2f67ec1fba00bf51f0a3e5d6bad
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-01-13 08:11:23 +0900

    P186 リスト5-3〜6 sqlite設定＆DBクラスを使ってデータ取得

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,18 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\HelloRequest;
-use Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        return view('hello.index', ['msg'=>'フォームを入力してください。']);
-    }
-
-    public function post(HelloRequest $request)
-    {
-        return view('hello.index', ['msg'=>'正しく入力されました！']);
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,30 +8,17 @@
 @endsection
 
 @section('content')
-    <p>{{ $msg }}</p>
-    @if (count($errors) > 0)
-    <p>入力に問題があります。再入力してください。</p>
-    @endif
-    <form actoin="/hello" method="post">
-        <table>
-            @csrf
-            @error('name')
-            <tr><th>ERROR</th><td>{{ $message }}</td></th></tr>
-            @enderror
-            <tr><th>name: </th><td><input type="text" name="name" value="{{ old('mail') }}"></th></tr>
-            @error('mail')
-            <tr><th>ERROR</th><td>{{ $message }}</td></th></tr>
-            @enderror
-            <tr><th>mail: </th><td><input type="text" name="mail" value="{{ old('mail') }}"></td></th></tr>
-            @error('age')
-            <tr><th>ERROR</th><td>{{ $message }}</td></th></tr>
-            @enderror
-            <tr><th>age: </th><td><input type="text" name="age" value="{{ old('age') }}"></td></th></tr>
-            <tr><th></th><td><input type="submit" vlaue="send"></td></tr>
-        </table>
-    </form>
+    <table>
+        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{$item->name}}</td>
+                <td>{{$item->mail}}</td>
+                <td>{{$item->age}}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
-
 @section('footer')
 copyright 2020 tuyano.
 @endsection
