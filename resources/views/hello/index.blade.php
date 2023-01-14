efd3877af869b53ccc2597c10af8c82e4a566e33
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-01-14 16:48:57 +0900

    P202 リスト5-14〜16 DB::deleteを使った削除

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,15 +8,16 @@
 @endsection
 
 @section('content')
-    <form action="/hello/add" method="post">
-        <table>
-            @csrf
-            <tr><th>name: </th><td><input type="text" name="name"></td></tr>
-            <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
-            <tr><th>age: </th><td><input type="text" name="age"></td></tr>
-            <tr><th></th><td><input type="submit" value="send"></td></tr>
-        </table>
-    </form>
+    <table>
+        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{$item->name}}</td>
+                <td>{{$item->mail}}</td>
+                <td>{{$item->age}}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
 
 @section('footer')
