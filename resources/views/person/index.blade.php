0a446b6f128982963d060b9c610bbdc0b862cda8
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-01-18 08:13:08 +0900

    P244 リストP6-7〜9 findメソッド使用

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -9,15 +9,15 @@
 
 @section('content')
     <table>
-        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
-        @foreach ($items as $item)
-            <tr>
-                <td>{{$item->getData()}}</td>
-            </tr>
-        @endforeach
+    <tr><th>Data</th></tr>
+    @foreach ($items as $item)
+        <tr>
+            <td>{{$item->getData()}}</td>
+        </tr>
+    @endforeach
     </table>
 @endsection
-
+ 
 @section('footer')
 copyright 2020 tuyano.
 @endsection
