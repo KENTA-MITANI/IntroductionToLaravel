3234b0d6075e1fc3b01e7da8f77b615d2ef7a61e
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-01-21 19:25:34 +0900

    P255 リスト6-18〜21 モデルの保存処理

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -9,32 +9,20 @@ use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
-    use HasFactory;
+
+    protected $guarded = array('id');
+
+    // created_at,updated_atを除いて登録
+    public $timestamps = false;
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
 
     public function getData()
     {
         return $this->id . '：' .  $this->name . '（' . $this->age . '）';
-    }
-
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where('name', $str);
-    }
-
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where('age', '>=', $n);
-    }
-
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where('age', '<=', $n);
-    }
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope(new ScopePerson);
     }
 }
