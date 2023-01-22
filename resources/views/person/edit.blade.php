fffc74836b4e17a16e151b8b78823abeb6af5555
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-01-22 12:04:47 +0900

    P262 リスト6-23〜24 データ更新

--- resources/views/person/edit.blade.php
+++ resources/views/person/edit.blade.php
@@ -17,12 +17,13 @@
         </ul>
     </div>
     @endif
-    <form action="/person/add" method="post">
+    <form action="/person/edit" method="post">
     <table>
         @csrf
-        <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
-        <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
-        <tr><th>age: </th><td><input type="number" name="age" value="{{old('age')}}"></td></tr>
+        <input type="hidden" name="id" value="{{$form->id}}">
+        <tr><th>name: </th><td><input type="text" name="name" value="{{$form->name}}"></td></tr>
+        <tr><th>mail: </th><td><input type="text" name="mail" value="{{$form->mail}}"></td></tr>
+        <tr><th>age: </th><td><input type="number" name="age" value="{{$form->age}}"></td></tr>
         <tr><th></th><td><input type="submit" value="send"></td></tr>
     </table>
     </form>
