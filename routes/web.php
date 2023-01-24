6c4af1a126de14354d128a8caf2b1e1ebdc6e6c1
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-01-25 08:32:13 +0900

    P302 リスト7-10〜12

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,5 @@ Route::get('board/add', 'App\Http\Controllers\BoardController@add');
 Route::post('board/add', 'App\Http\Controllers\BoardController@create');
 
 Route::resource('rest', 'App\Http\Controllers\RestappController');
+
+Route::get('hello/rest', 'App\Http\Controllers\HelloController@rest');
