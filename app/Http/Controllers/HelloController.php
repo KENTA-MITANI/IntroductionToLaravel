8b921c1913bcecba161ee0ce283e109b9e4b71cb
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-01-28 12:04:17 +0900

    P312 リスト7-20〜21 simplePaginate実装

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Person;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::table('people')->get();
+        $items = DB::table('people')->simplePaginate(5);
         return view('hello.index', ['items' => $items]);
     }
 
