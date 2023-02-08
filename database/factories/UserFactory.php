e1662526846705d3a575d2e9ae8c767c970a2427
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-02-09 07:43:15 +0900

    P336 リスト7-35〜42 ユニットテスト

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+// use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 /**
---
+++ tests/Feature/HelloTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class HelloTest extends TestCase
+{
+    // DBの初期化とマイグレーションを行っているので必須！！！
+    // test実行初回時、テーブルが一切ないので本記述がなければ「no such table」となる 
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testHello()
+    {
+        $this->assertTrue(true);
+
+        $response = $this->get('/');
+        $response->assertStatus(200);
+
+        $response = $this->get('/hello');
+        $response->assertStatus(302);
+
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/hello');
+        $response->assertStatus(200);
+
+        $response = $this->get('/no_route');
+        $response->assertStatus(404);
+    }
+}
